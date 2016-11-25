048a1210838ad71fd4ff231b73f66032b39087df
zecst19 <zecst19@gmail.com>
2016-11-25 10:30:20 +0000

    aula

---
+++ sql/Data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, 'Joao', 'Castro','pandasarehere@gmail.com','photou1','Salsichas420');
+
+insert into restaurant values(1,'TidBit','italian restaurant and pizzeria', 10, 'photor1');
--- sql/Tables.sql
+++ sql/Tables.sql
@@ -3,15 +3,21 @@ CREATE TABLE user(id_user int primary key,
 					first_name char(50),
 					last_name char(50),
 					email char(50),
-					photo char(50),);
+					photo char(50),
+					password char(32));
 					
 CREATE TABLE restaurant(id_restaurant int primary key, 
 						name char(50), 
 						description char(50), 
 						rate int,
-						photo char(50),);
+						photo char(50));
 					
 CREATE TABLE review(id_review int primary key,
+					id_user references user(id_user),
+					id_restaurant references user(id_restaurant),
 					rate int,
-					commment char(150),
-					);
+					commment char(150));
+					
+CREATE TABLE location(id_location int primary key,
+						country char(50),
+						city char(50));
