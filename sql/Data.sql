ea3078df7444d1ffd443aa386c8efc5c3e882bf5
zecst19 <zecst19@gmail.com>
2016-11-29 17:14:50 +0000

    update na database

--- sql/Data.sql
+++ sql/Data.sql
@@ -1,3 +1,40 @@
-insert into user values(1, 'Joao', 'Castro','pandasarehere@gmail.com','photou1','Salsichas420');
+insert into user values(1, 'Joao', 'Castro','pandasarehere@gmail.com','photo1','Salsichas420');
+insert into user values(2, 'Tiago', 'Pacheco','avidadepi@gmail.com','photo2','Bagacocommel2');
+insert into user values(3, 'Manuel', 'Gomes','timonepumba@gmail.com','photo3','Altibor5');
+insert into user values(4, 'Vasco', 'Ferreira','calcitrinmd@gmail.com','photo4','Papibaquigrafos1');
+insert into user values(5, 'Alcino', 'Mendes','victormoses@gmail.com','photo5','4Governo');
+insert into user values(6, 'Rodrigo', 'Santoro','monstrosvsaliens@gmail.com','photo6','VaiSer4');
+insert into user values(7, 'Joao', 'Gonçalves','pois.pois@gmail.com','photo7','Pois2');
+insert into user values(8, 'Nuno', 'Assunção','montalegre@gmail.com','photo8','Pelopidas1');
+insert into user values(9, 'David', 'Magalhães','binhotinto@gmail.com','photo9','Salsichas430');
+insert into user values(10, 'Rui', 'Costa','benfas@gmail.com','photo10','AndreAlmeida19');
 
-insert into restaurant values(1,'TidBit','italian restaurant and pizzeria', 10, 'photor1');
+insert into restaurant values(1,'TidBit','italian restaurant and pizzeria', 10, 'photo1');
+insert into restaurant values(2,'Casa de Pasto Zé de Ver','tasca', 10, 'photo2');
+insert into restaurant values(3,'Condado','italian restaurant and pizzeria', 10, 'photo3');
+insert into restaurant values(4,'Mumadona','italian restaurant and pizzeria', 10, 'photo4');
+insert into restaurant values(5,'TidBit','italian restaurant and pizzeria', 10, 'photo5');
+insert into restaurant values(6,'TidBit','italian restaurant and pizzeria', 10, 'photo6');
+insert into restaurant values(7,'TidBit','italian restaurant and pizzeria', 10, 'photo7');
+insert into restaurant values(8,'TidBit','italian restaurant and pizzeria', 10, 'photo8');
+insert into restaurant values(9,'TidBit','italian restaurant and pizzeria', 10, 'photo9');
+insert into restaurant values(10,'TidBit','italian restaurant and pizzeria', 10, 'photo10');
+
+insert into review values(1,1,1,10,'muito bicho');
+insert into review values(2,1,1,10,'muito bicho');
+insert into review values(3,1,1,10,'muito bicho');
+insert into review values(4,1,1,10,'muito bicho');
+insert into review values(5,1,1,10,'muito bicho');
+insert into review values(6,1,1,10,'muito bicho');
+insert into review values(7,1,1,10,'muito bicho');
+insert into review values(8,1,1,10,'muito bicho');
+insert into review values(9,1,1,10,'muito bicho');
+insert into review values(10,1,1,10,'muito bicho');
+
+insert into location values(1,'Portugal','Porto');
+insert into location values(2,'Portugal','Guimarães';
+insert into location values(3,'Portugal','Lisboa');
+
+insert into location_restaurant values(1,1);
+insert into location_restaurant values(2,3);
+insert into location_restaurant values(2,4);
--- sql/Tables.sql
+++ sql/Tables.sql
@@ -10,14 +10,17 @@ CREATE TABLE restaurant(id_restaurant int primary key,
 						name char(50), 
 						description char(50), 
 						rate int,
-						photo char(50));
+						photo char(50);
 					
 CREATE TABLE review(id_review int primary key,
 					id_user references user(id_user),
-					id_restaurant references user(id_restaurant),
+					id_restaurant references restaurant(id_restaurant),
 					rate int,
 					commment char(150));
 					
 CREATE TABLE location(id_location int primary key,
 						country char(50),
 						city char(50));
+						
+CREATE TABLE location_restaurant(id_location references location(id_location),
+								id_restaurant references restaurant(id_restaurant));
