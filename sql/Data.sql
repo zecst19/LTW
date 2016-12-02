2607140fa04b76f9c4ad782eadc73073c60fb77a
zecst19 <zecst19@gmail.com>
2016-12-02 09:05:50 +0000

    removi repetidos

--- sql/Data.sql
+++ sql/Data.sql
@@ -13,23 +13,8 @@ insert into restaurant values(1,'TidBit','italian restaurant and pizzeria', 10, 
 insert into restaurant values(2,'Casa de Pasto Zé de Ver','tasca', 10, 'photo2');
 insert into restaurant values(3,'Condado','italian restaurant and pizzeria', 10, 'photo3');
 insert into restaurant values(4,'Mumadona','italian restaurant and pizzeria', 10, 'photo4');
-insert into restaurant values(5,'TidBit','italian restaurant and pizzeria', 10, 'photo5');
-insert into restaurant values(6,'TidBit','italian restaurant and pizzeria', 10, 'photo6');
-insert into restaurant values(7,'TidBit','italian restaurant and pizzeria', 10, 'photo7');
-insert into restaurant values(8,'TidBit','italian restaurant and pizzeria', 10, 'photo8');
-insert into restaurant values(9,'TidBit','italian restaurant and pizzeria', 10, 'photo9');
-insert into restaurant values(10,'TidBit','italian restaurant and pizzeria', 10, 'photo10');
 
 insert into review values(1,1,1,10,'muito bicho');
-insert into review values(2,1,1,10,'muito bicho');
-insert into review values(3,1,1,10,'muito bicho');
-insert into review values(4,1,1,10,'muito bicho');
-insert into review values(5,1,1,10,'muito bicho');
-insert into review values(6,1,1,10,'muito bicho');
-insert into review values(7,1,1,10,'muito bicho');
-insert into review values(8,1,1,10,'muito bicho');
-insert into review values(9,1,1,10,'muito bicho');
-insert into review values(10,1,1,10,'muito bicho');
 
 insert into location values(1,'Portugal','Porto');
 insert into location values(2,'Portugal','Guimarães';
