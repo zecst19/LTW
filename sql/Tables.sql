9ed43f4a171b5a6c33ae9a1845abaed915dba416
zecst19 <zecst19@gmail.com>
2016-12-10 23:23:28 +0000

    added more restaurants and photos

--- sql/Tables.sql
+++ sql/Tables.sql
@@ -28,21 +28,33 @@ CREATE TABLE location_restaurant(id_location int REFERENCES location(id_location
 								
 								
 								
-insert into user values(1, 'Joao', 'Castro','pandasarehere@gmail.com','photo1','Salsichas420');
-insert into user values(2, 'Tiago', 'Pacheco','avidadepi@gmail.com','photo2','Bagacocommel2');
-insert into user values(3, 'Manuel', 'Gomes','timonepumba@gmail.com','photo3','Altibor5');
-insert into user values(4, 'Vasco', 'Ferreira','calcitrinmd@gmail.com','photo4','Papibaquigrafos1');
-insert into user values(5, 'Alcino', 'Mendes','victormoses@gmail.com','photo5','4Governo');
-insert into user values(6, 'Rodrigo', 'Santoro','monstrosvsaliens@gmail.com','photo6','VaiSer4');
-insert into user values(7, 'Joao', 'Gonçalves','pois.pois@gmail.com','photo7','Pois2');
-insert into user values(8, 'Nuno', 'Assunção','montalegre@gmail.com','photo8','Pelopidas1');
-insert into user values(9, 'David', 'Magalhães','binhotinto@gmail.com','photo9','Salsichas430');
-insert into user values(10, 'Rui', 'Costa','benfas@gmail.com','photo10','AndreAlmeida19');
+insert into user values(1, 'Joao', 'Castro','pandasarehere@gmail.com','uphoto1','Salsichas420');
+insert into user values(2, 'Tiago', 'Pacheco','avidadepi@gmail.com','uphoto2','Bagacocommel2');
+insert into user values(3, 'Manuel', 'Gomes','timonepumba@gmail.com','uphoto3','Altibor5');
+insert into user values(4, 'Vasco', 'Ferreira','calcitrinmd@gmail.com','uphoto4','Papibaquigrafos1');
+insert into user values(5, 'Alcino', 'Mendes','victormoses@gmail.com','uphoto5','4Governo');
+insert into user values(6, 'Rodrigo', 'Santoro','monstrosvsaliens@gmail.com','uphoto6','VaiSer4');
+insert into user values(7, 'Joao', 'Gonçalves','pois.pois@gmail.com','uphoto7','Pois2');
+insert into user values(8, 'Nuno', 'Assunção','montalegre@gmail.com','uphoto8','Pelopidas1');
+insert into user values(9, 'David', 'Magalhães','binhotinto@gmail.com','uphoto9','Salsichas430');
+insert into user values(10, 'Rui', 'Costa','benfas@gmail.com','uphoto10','AndreAlmeida19');
 
 insert into restaurant values(1,'TidBit','italian restaurant and pizzeria', 10, 'photo1');
 insert into restaurant values(2,'Casa de Pasto Zé de Ver','tasca', 8, 'photo2');
 insert into restaurant values(3,'Condado','italian restaurant and pizzeria', 5, 'photo3');
-insert into restaurant values(4,'Mumadona','italian restaurant and pizzeria', 7, 'photo4');
+insert into restaurant values(4,'Mumadona','vinho e castanhas cruas', 7, 'photo4');
+insert into restaurant values(5,'Pinguim','jarros de receita', 6, 'photo5');
+insert into restaurant values(6,'Adega Amarela','tudo à descriçao', 9, 'photo6');
+insert into restaurant values(7,'Lusíada','a real', 10, 'photo7');
+insert into restaurant values(8,'Bufete Rapide','lusiada com as paredes escritas', 10, 'photo8');
+insert into restaurant values(9,'Sala 141','sandes e assim', 8, 'photo9');
+insert into restaurant values(10,'Taberna Belga','esperar 45 minutos e francesinhas', 6, 'photo10');
+insert into restaurant values(11,'Yuko','melhores francesinhas', 9, 'photo11');
+insert into restaurant values(12,'Taberna Londrina','mais francesinhas', 8, 'photo12');
+insert into restaurant values(13,'Portuscale','caro', 8, 'photo13');
+insert into restaurant values(14,'Luar','sandes e assim', 5, 'photo14');
+insert into restaurant values(15,'Honorato','hamburguers bicho', 9, 'photo15');
+insert into restaurant values(16,'Kebabs do Raza','melhores kebabs do mundo', 10, 'photo16');
 
 insert into review values(1,1,1,10,'muito bicho');
 insert into review values(2,2,2,10,'pouco bicho');
@@ -53,7 +65,23 @@ insert into review values(5,1,1,10,'omfg bicho');
 insert into location values(1,'Portugal','Porto');
 insert into location values(2,'Portugal','Guimarães');
 insert into location values(3,'Portugal','Lisboa');
+insert into location values(4,'Portugal','Braga');
+insert into location values(5,'Portugal','Santo Tirso');
+insert into location values(6,'Portugal','Arouca');
 
 insert into location_restaurant values(1,1);
 insert into location_restaurant values(2,3);
 insert into location_restaurant values(2,4);
+insert into location_restaurant values(2,5);
+insert into location_restaurant values(6,2);
+insert into location_restaurant values(5,6);
+insert into location_restaurant values(1,7);
+insert into location_restaurant values(1,8);
+insert into location_restaurant values(2,9);
+insert into location_restaurant values(4,10);
+insert into location_restaurant values(1,11);
+insert into location_restaurant values(2,12);
+insert into location_restaurant values(1,13);
+insert into location_restaurant values(1,14);
+insert into location_restaurant values(3,15);
+insert into location_restaurant values(5,16);
