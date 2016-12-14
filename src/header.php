f9c30b53c3815229a6e2889ac7fc9bd7e9adfb74
zecst19 <zecst19@gmail.com>
2016-12-14 22:24:19 +0000

    changed header

--- src/header.php
+++ src/header.php
@@ -9,6 +9,6 @@
 </head>
 <body>
 <h1><a href="./HomePage.php"  style="text-decoration:none">Sublime</a></h1>
-
+<h4><a href="./loginScreen.php"  style="text-decoration:none">Login/Register</a></h4>
 </body>
 </html>
